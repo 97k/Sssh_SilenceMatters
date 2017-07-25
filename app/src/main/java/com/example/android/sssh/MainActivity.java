044a5c1ffce887dbc3fc9775e1a893d9ddd33f65
Aditya Kaushik <adityakaushik471@gmail.com>
2017-07-25 23:54:43 +0530

    Building the basic UI!

---
+++ app/src/main/java/com/example/android/sssh/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.android.sssh;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     *
+     * @param menu The menu to inflate.
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_delete:
+                //TODO perform the delete option here.
+                return true;
+
+            case R.id.configure_sssh:
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
